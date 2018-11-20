53fc9ebc32d77da67ff206d5591149660849ddae
John Blischak <jdblischak@gmail.com>
2018-11-19 22:40:46 -0500

    Download code and only download missing data files.

--- download-data.sh
+++ download-data.sh
@@ -6,26 +6,28 @@ outdir=data/
 
 mkdir -p $outdir
 
+wget_cmd="wget -P $outdir --no-clobber"
+
 echo "Downloading sample attributes"
-wget -P $outdir https://storage.googleapis.com/gtex_analysis_v6p/annotations/GTEx_Data_V6_Annotations_SampleAttributesDS.txt
+$wget_cmd https://storage.googleapis.com/gtex_analysis_v6p/annotations/GTEx_Data_V6_Annotations_SampleAttributesDS.txt
 
 echo "Downloading gene counts"
-wget -P $outdir https://storage.googleapis.com/gtex_analysis_v6p/rna_seq_data/GTEx_Analysis_v6p_RNA-seq_RNA-SeQCv1.1.8_gene_reads.gct.gz
+$wget_cmd https://storage.googleapis.com/gtex_analysis_v6p/rna_seq_data/GTEx_Analysis_v6p_RNA-seq_RNA-SeQCv1.1.8_gene_reads.gct.gz
 
 echo "Downloading gene annotations"
-wget -P $outdir https://storage.googleapis.com/gtex_analysis_v6p/reference/gencode.v19.genes.v6p_model.patched_contigs.gtf.gz
+$wget_cmd https://storage.googleapis.com/gtex_analysis_v6p/reference/gencode.v19.genes.v6p_model.patched_contigs.gtf.gz
 
 echo "Downloading subject phenotypes"
-wget -P $outdir https://storage.googleapis.com/gtex_analysis_v6p/annotations/GTEx_Data_V6_Annotations_SubjectPhenotypesDS.txt
+$wget_cmd https://storage.googleapis.com/gtex_analysis_v6p/annotations/GTEx_Data_V6_Annotations_SubjectPhenotypesDS.txt
 
 echo "Downloading Eisenberg & Levanon 2013 housekeeping genes"
-wget -P $outdir http://www.tau.ac.il/~elieis/HKG/HK_genes.txt
+$wget_cmd http://www.tau.ac.il/~elieis/HKG/HK_genes.txt
 
 echo "Downloading NCBI-to-Ensembl gene mappings"
-wget -P $outdir ftp://ftp.ncbi.nih.gov/gene/DATA/gene2ensembl.gz
+$wget_cmd ftp://ftp.ncbi.nih.gov/gene/DATA/gene2ensembl.gz
 
 echo "Downloading Lin2017 housekeeping genes"
-wget -P $outdir https://raw.githubusercontent.com/dcgerard/mouthwash_sims/master/Data/h-scHKgenes.csv
+$wget_cmd https://raw.githubusercontent.com/dcgerard/mouthwash_sims/master/Data/h-scHKgenes.csv
 
 echo "Downloading Ensembl gene IDs for Lin2017 housekeeping genes"
-wget -P $outdir https://raw.githubusercontent.com/dcgerard/mouthwash_sims/master/Data/lin_hk_genes.csv
+$wget_cmd https://raw.githubusercontent.com/dcgerard/mouthwash_sims/master/Data/lin_hk_genes.csv
