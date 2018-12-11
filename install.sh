537d6f909748b93a77ecacc867f20df64a52a1af
John Blischak <jdblischak@gmail.com>
2018-12-11 14:30:13 -0500

    Install dscrutils with conda.
    https://github.com/jdblischak/jdb-conda/blob/master/recipes/r-dscrutils/meta.yaml

--- install.sh
+++ install.sh
@@ -15,11 +15,3 @@ conda info -a
 conda install --file requirements/misc \
               --file requirements/python \
               --file requirements/r
-
-# Install dscrutils from GitHub repo subdirectory
-wget -O /tmp/dsc.zip https://github.com/stephenslab/dsc/archive/master.zip
-unzip /tmp/dsc.zip -d /tmp/
-R CMD build --no-build-vignettes --no-manual /tmp/dsc-master/dscrutils/
-mv dscrutils_*.tar.gz /tmp/
-R CMD INSTALL /tmp/dscrutils_*.tar.gz
-rm -r /tmp/dsc*
