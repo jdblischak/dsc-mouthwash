045dad955d22fe8f95e0e80692d173301fac6e8a
John Blischak <jdblischak@gmail.com>
2019-03-01 09:57:29 -0500

    Use conda 4.3.27 for faster installation.

--- install.sh
+++ install.sh
@@ -4,11 +4,14 @@ set -eux
 # Configure conda
 conda config --set always_yes yes
 conda config --set changeps1 no
+conda config --set auto_update_conda no
+
+# Configure channels
 conda config --add channels jdblischak
 conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
-conda update -q conda
+
 conda info -a
 
 # Install conda packages
